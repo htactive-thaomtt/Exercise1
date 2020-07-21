0810a7298ab6b932215e535519b30037bd4efc86
Thu Thao <thaomtt@htactive.com>
2020-07-21 13:17:34 +0700

    create localStorage

--- src/component/ListCard.js
+++ src/component/ListCard.js
@@ -1,17 +1,20 @@
 import { CloseOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Card, Col, Form, Input, Row, Popover } from "antd";
+import { Button, Card, Col, Form, Input, Popover, Row } from "antd";
 import React, { useRef, useState } from "react";
-import ListTask from "./ListTask";
-import AddTask from "./AddTask";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewCard, addNewTask, removeCard } from "../actions/task";
+import AddTask from "./AddTask";
+import ListTask from "./ListTask";
 const ListCart = () => {
   const [displayForm, setdisplayForm] = useState(false);
-  const [displayButton, setdisplayButton] = useState({visible: false, key: ''});
-  // const [displayButton, setdisplayButton] = useState(false); 
+  const [displayButton, setdisplayButton] = useState({
+    visible: false,
+    key: "",
+  });
   const valueRef = useRef(null);
 
   const list = useSelector((state) => state.tasks);
+
   const dispatch = useDispatch();
 
   const handleToAdd = async () => {
@@ -35,21 +38,24 @@ const ListCart = () => {
     dispatch(action);
   };
   const handleClick = (key) => {
-    setdisplayButton({visible: !displayButton.visible, key: key});
-    // setdisplayButton(!displayButton);
+    setdisplayButton({ visible: !displayButton.visible, key: key });
   };
   const handleRemove = (key) => {
     const action = removeCard(key);
     dispatch(action);
-    setdisplayButton({visible: false, key: ''});    
+    setdisplayButton({ visible: false, key: "" });
   };
   return (
     <Row className="list-card" style={{ margin: "20px" }}>
       {list.map((item, key) => (
-        <Col span={6} key={key} style={{marginRight: '20px'}}>
+        <Col span={6} key={key} style={{ marginRight: "20px" }}>
           <Card
-            headStyle={{ fontWeight: "bolder", fontSize: "20px", backgroundColor: '#d9d9d9' }}
-            bodyStyle={{backgroundColor: '#d9d9d9'}}
+            headStyle={{
+              fontWeight: "bolder",
+              fontSize: "20px",
+              backgroundColor: "#d9d9d9",
+            }}
+            bodyStyle={{ backgroundColor: "#d9d9d9" }}
             size="small"
             key={key}
             title={item.title}
@@ -59,9 +65,10 @@ const ListCart = () => {
                 // eslint-disable-next-line jsx-a11y/anchor-is-valid
                 content={<a onClick={() => handleRemove(key)}>Remove</a>}
                 trigger="click"
-                visible={displayButton.key === key ? displayButton.visible : false}
-                // visible={displayButton}
-                onVisibleChange={()=>handleClick(key)}
+                visible={
+                  displayButton.key === key ? displayButton.visible : false
+                }
+                onVisibleChange={() => handleClick(key)}
               >
                 <Button style={{ border: "none" }}>...</Button>
               </Popover>
@@ -73,11 +80,15 @@ const ListCart = () => {
           </Card>
         </Col>
       ))}
-      <Col span={6}>
+      <Col span={5} style={{marginTop: '40px'}}>
         {displayForm ? (
           <Form
             layout="inline"
-            style={{ margin: "40px", backgroundColor:'#d9d9d9', padding: '10px' }}
+            style={{
+              backgroundColor: "#d9d9d9",
+              padding: "10px",
+              justifyContent: 'center'
+            }}
             onFinish={(values) => handleAddCard(values)}
           >
             <Form.Item
@@ -88,14 +99,30 @@ const ListCart = () => {
             </Form.Item>
             <Form.Item>
               <Button
-                style={{ color: "#ffffff", backgroundColor: "#00b33c", marginTop: '5px', paddingLeft: '20px', paddingRight: '20px'}}
+                style={{
+                  color: "#ffffff",
+                  backgroundColor: "#00b33c",
+                  marginTop: "5px",
+                  paddingLeft: "20px",
+                  paddingRight: "20px",
+                }}
                 htmlType="submit"
               >
                 Add
               </Button>
             </Form.Item>
             <Form.Item>
-              <Button size = 'small' style={{ border: "none" , backgroundColor: 'transparent', marginTop: '5px', fontSize: '20px', fontWeight: 'bold'}} onClick={handleExit}>
+              <Button
+                size="small"
+                style={{
+                  border: "none",
+                  backgroundColor: "transparent",
+                  marginTop: "5px",
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                }}
+                onClick={handleExit}
+              >
                 <CloseOutlined />
               </Button>
             </Form.Item>
@@ -104,7 +131,11 @@ const ListCart = () => {
           <Button
             className="button-add"
             onClick={handleToAdd}
-            style={{ marginLeft: "40px", paddingLeft: '20px', paddingRight: '20px'}}
+            style={{
+              marginLeft: "40px",
+              paddingLeft: "20px",
+              paddingRight: "20px",
+            }}
           >
             <PlusOutlined />
             Add another list
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,10 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers";
+import { saveState, loadState } from './localStorage';
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+store.subscribe(() => {
+    saveState(store.getState());
+})
 export default store;
